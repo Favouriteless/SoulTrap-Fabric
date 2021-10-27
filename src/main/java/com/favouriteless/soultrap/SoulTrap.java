71e398efd89b7ff95ef54515ce072800a1af6cae
Favouriteless <62647254+Favouriteless@users.noreply.github.com>
2021-10-27 20:19:33 +0100

    Copyright notices, initial setup

--- src/main/java/com/favouriteless/soultrap/SoulTrap.java
+++ src/main/java/com/favouriteless/soultrap/SoulTrap.java
@@ -1,21 +1,36 @@
-package net.fabricmc.example;
+/*
+ * Copyright (c) 2021. Favouriteless
+ * SoulTrap-Fabric, a minecraft mod.
+ * GNU GPLv3 License
+ *
+ *     This file is part of SoulTrap-Fabric.
+ *
+ *     SoulTrap-Fabric is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     SoulTrap-Fabric is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with SoulTrap-Fabric.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.favouriteless.soultrap;
 
 import net.fabricmc.api.ModInitializer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class ExampleMod implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LogManager.getLogger("modid");
+public class SoulTrap implements ModInitializer {
+
+	public static final Logger LOGGER = LogManager.getLogger("soultrap");
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
 		LOGGER.info("Hello Fabric world!");
 	}
 }
